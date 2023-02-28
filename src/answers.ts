a6c27f324e0adb60cfeac6b31f15effd80b8e25d
Luis de la Vega Lopez <luisr.delavega@gmail.com>
2023-02-28 11:26:34 -0500

    More problems related to Arrays and Hash Tables

--- src/answers.ts
+++ src/answers.ts
@@ -1,15 +1,14 @@
 import { IAnimal } from './index';
 
-// --- Answer to Problem #1 ---
-
-// 1
+// #region --- Answer to Problem #1 ---
+// Part 1
 export function duplicate<T>(param: T[]) {
   return param.concat(param);
 }
 
 export const duplicateAlt = <T>(param: T[]) => [...param, ...param];
 
-// 2
+// Part 2
 export const revDuplicate = <T>(param: T[]) => {
   const arr = [...param]; // alternative -> param.slice(0);
   arr.reverse();
@@ -21,9 +20,9 @@ export const revDuplicateAlt = <T>(param: T[]) => [
   ...param,
   ...[...param].reverse()
 ];
+//#endregion
 
-// --- Answer to Problem #2 ---
-
+//#region --- Answer to Problem #2 ---
 export function groupByKey<T>(param: T[], groupKey: keyof T) {
   return param.reduce((acc, currentValue) => {
     const dept = currentValue[groupKey];
@@ -37,9 +36,9 @@ export function groupByKey<T>(param: T[], groupKey: keyof T) {
     return acc;
   }, {} as any);
 }
+//#endregion
 
-// --- Answer to Problem #3 ---
-
+//#region --- Answer to Problem #3 ---
 export const firstFactorial = (num: number): number => {
   if (num === 1) {
     return num;
@@ -49,9 +48,9 @@ export const firstFactorial = (num: number): number => {
 
   return result;
 };
+//#endregion
 
-// --- Answer to Problem #4 ---
-
+//#region --- Answer to Problem #4 ---
 export const howManyOfType = (data: IAnimal[]) => {
   return data.reduce((acc, currentVal) => {
     const { type } = currentVal;
@@ -79,8 +78,9 @@ export const howManyHaveCollar = (data: IAnimal[]) => {
     return acc;
   }, {} as any);
 };
+//#endregion
 
-// --- Answer to Problem #5 ---
+//#region --- Answer to Problem #5 ---
 
 export const pipe =
   (...rest: ((x: any) => any)[]) =>
@@ -99,31 +99,47 @@ export function pipeEs5() {
     }, value);
   };
 }
+//#endregion
 
-// --- Answer to Problem #6 ---
+//#region --- Answer to Problem #6 ---
 
+/**
+ * This solution is for when they expicitly ask you not to use
+ * any built in array methods.
+ */
 export const reverseString = (str: string) => {
-  if (!str) return '';
+  if (!str) return str;
 
   const strArray = str.split('');
   const midPoint = Math.ceil(strArray.length / 2);
 
   for (
-    let forwardMovingIndex = 0, backwardsMovingIndex = strArray.length - 1;
-    forwardMovingIndex < midPoint;
-    forwardMovingIndex++, backwardsMovingIndex--
+    let forwardIndex = 0, backwardsIndex = strArray.length - 1;
+    forwardIndex < midPoint;
+    forwardIndex++, backwardsIndex--
   ) {
     // The most common way is to declare this here 👇
-    // const backwardsMovingIndex = strArray.length - forwardMovingIndex - 1;
-    const charAtBackwardsMovingIndex = strArray[backwardsMovingIndex];
-    strArray[backwardsMovingIndex] = strArray[forwardMovingIndex];
-    strArray[forwardMovingIndex] = charAtBackwardsMovingIndex;
+    // const backwardsIndex = strArray.length - forwardMovingIndex - 1;
+    const tempChar = strArray[backwardsIndex];
+    strArray[backwardsIndex] = strArray[forwardIndex];
+    strArray[forwardIndex] = tempChar;
   }
 
   return strArray.join('');
 };
 
-// --- Answer to Problem #7 ---
+export const reverseStringWithArrSort = (str: string) => {
+  if (!str.length) return str;
+
+  return str
+    .split('')
+    .sort(() => 1)
+    .join('');
+};
+
+//#endregion
+
+//#region --- Answer to Problem #7 ---
 
 // Best: O(n*m). Worst: basically O(n^2)
 export const logPairs = <T>(arr: T[]) => {
@@ -134,7 +150,9 @@ export const logPairs = <T>(arr: T[]) => {
   }
 };
 
-// --- Answer to Problem #8 ---
+//#endregion
+
+//#region --- Answer to Problem #8 ---
 
 // More readable, less efficient O(n*m)
 export const containsCommonValue = (arr1: string[], arr2: string[]) =>
@@ -176,7 +194,9 @@ export const containsCommonValueNoArrayFuncNoSet = (
   return false;
 };
 
-// --- Answer to Problem #9 ---
+//#endregion
+
+//#region --- Answer to Problem #9 ---
 
 export const findPairWithSum = (arr: number[], sum: number) => {
   const setFromArr = new Set(arr);
@@ -191,3 +211,496 @@ export const findPairWithSum = (arr: number[], sum: number) => {
 
   return [];
 };
+
+//#endregion
+
+//#region --- Answer to Problem #10 ---
+
+export const mergeSortedArrays = (arr1: number[], arr2: number[]) => {
+  if (!arr1.length) {
+    return arr2;
+  } else if (!arr2.length) {
+    return arr1;
+  }
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+};
+
+export const mergeSortedArraysNoSortFunc = (arr1: number[], arr2: number[]) => {
+  if (!arr1.length) {
+    return arr2;
+  } else if (!arr2.length) {
+    return arr1;
+  }
+
+  const result: number[] = [];
+  let index1 = 0;
+  let index2 = 0;
+
+  while (index1 < arr1.length) {
+    const arrr1Value = arr1[index1];
+    const arrr2Value = arr2[index2];
+
+    if (typeof arrr2Value === 'undefined' || arrr1Value < arrr2Value) {
+      result.push(arrr1Value);
+      ++index1;
+    } else {
+      result.push(arrr2Value);
+      ++index2;
+    }
+  }
+
+  while (index2 < arr2.length) {
+    result.push(arr2[index2]);
+    ++index2;
+  }
+
+  return result;
+};
+
+//#endregion
+
+//#region --- Answer to Problem #11 ---
+
+// https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+// globalMax -> -Infinity
+// index -> 0
+//          👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] + localMax -> -2 + 0 = -2
+// localMax -> Larger between nums[index] & previousSum -> -2 > -2 = -2
+// IF localMax (-2) > globalMax (-Infinity) -> YES
+// globalMax -> -2
+
+// index -> 1
+//             👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] + localMax -> -2 + 1 = -1
+// localMax -> Larger between nums[index] (1) & previousSum (-1) -> 1
+// IF localMax (1) > globalMax (-2) -> YES
+//    globalMax -> 1
+
+// index -> 2
+//                 👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] (-3) + localMax (1) -> -2
+// localMax -> Larger between nums[index] (-3) & previousSum (-2) -> -2
+// IF localMax (-2) > globalMax (1) -> NO
+//    globalMax -> 1
+
+// index -> 3
+//                    👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] (4) + localMax (-2) -> 2
+// localMax -> Larger between nums[index] (4) & previousSum (2) -> 4
+// IF localMax (4) > globalMax (1) -> YES
+//    globalMax -> 4
+
+// index -> 4
+//                        👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] (-1) + localMax (4) -> 3
+// localMax -> Larger between nums[index] (-1) & previousSum (3) -> 3
+// IF localMax (3) > globalMax (4) -> NO
+//    globalMax -> 4
+
+// index -> 5
+//                           👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] (2) + localMax (3) -> 5
+// localMax -> Larger between nums[index] (2) & previousSum (5) -> 5
+// IF localMax (5) > globalMax (4) -> YES
+//    globalMax -> 5
+
+// index -> 6
+//                              👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] (1) + localMax (5) -> 6
+// localMax -> Larger between nums[index] (1) & previousSum (6) -> 6
+// IF localMax (6) > globalMax (5) -> YES
+//    globalMax -> 6
+
+// index -> 6
+//                                  👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] (-5) + localMax (6) -> 1
+// localMax -> Larger between nums[index] (-5) & previousSum (1) -> 1
+// IF localMax (1) > globalMax (6) -> NO
+//    globalMax -> 6
+
+// index -> 7
+//                                     👇
+// nums -> [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// previousSum -> nums[index] (4) + localMax (1) -> 5
+// localMax -> Larger between nums[index] (4) & previousSum (5) -> 5
+// IF localMax (5) > globalMax (6) -> NO
+//    globalMax -> 6
+
+export const maxSubArray = (nums: number[]): number => {
+  let localMax = 0;
+  let globalMax = -Infinity; // Set to -Infinity so that it's always less than localMax at first
+
+  for (let index = 0; index < nums.length; index++) {
+    const previousArraySum = nums[index] + localMax;
+
+    /**
+     * Check if the num and index is larger than the sum.
+     * If so, that means the there is a sub-array starting at that index
+     * that is larger than the one we've been following so far.
+     */
+    localMax = nums[index] > previousArraySum ? nums[index] : previousArraySum;
+
+    if (localMax > globalMax) {
+      globalMax = localMax;
+    }
+  }
+
+  return globalMax;
+};
+
+//#endregion
+
+//#region --- Answer to Problem #12 ---
+
+// nums -> [0, 1, 0, 3, 12]
+// movesNeeded = 2
+
+// index = 0
+//         👇
+// nums -> [0, 1, 0, 3, 12]
+// movesDone = 0
+// Check if 0 and movesDone < movesNeeded
+// Move the 0 -> [1, 0, 3, 12, 0]
+// movesDone++ -> 1
+// Don't increment index
+
+// movesDone = 1
+// index = 0
+// Check if 0 and movesDone < movesNeeded
+// Don't move anything -> [1, 0, 3, 12, 0]
+// Don't ioncrement movesDone
+// index++
+
+// movesDone = 1
+// index = 1
+// Check if 0 and movesDone < movesNeeded
+// Move the 0 -> [1, 3, 12, 0, 0]
+// movesDone++ -> 2
+// return here
+
+export const moveZeroes = (nums: number[]): void => {
+  if (nums.length <= 1) {
+    return;
+  }
+
+  let movesDone = 0;
+  const movesNeeded = nums.reduce((acc, currValue) => {
+    if (!currValue) {
+      ++acc;
+    }
+
+    return acc;
+  }, 0);
+
+  for (let index = 0; movesDone < movesNeeded; index++) {
+    if (!nums[index]) {
+      nums.splice(index, 1);
+      nums.push(0);
+      ++movesDone;
+      --index;
+    }
+  }
+};
+
+// https://medium.com/@rebeccahezhang/leetcode-283-move-zeroes-321293784283
+export const moveZeroesAlt = (nums: number[]): void => {
+  if (nums.length <= 1) {
+    return;
+  }
+
+  let index1 = 0;
+  const movesNeeded = nums.reduce((acc, currValue) => {
+    if (!currValue) {
+      ++acc;
+    }
+
+    return acc;
+  }, 0);
+
+  for (let index2 = 0; index1 < nums.length - movesNeeded; index2++) {
+    if (nums[index2]) {
+      nums[index1++] = nums[index2];
+    }
+  }
+
+  while (index1 < nums.length) {
+    nums[index1++] = 0;
+  }
+};
+
+//#endregion
+
+//#region --- Answer to Problem #13 ---
+
+export const containsDuplicate = (nums: number[]): boolean =>
+  new Set(nums).size !== nums.length;
+
+//#endregion
+
+//#region --- Answer to Problem #14 ---
+
+export const rotate = (nums: number[], k: number): void => {
+  if (!(k % nums.length)) {
+    return;
+  }
+
+  const shiftIndex = nums.length - (k % nums.length);
+  const tempArr = nums.slice(shiftIndex);
+  let popAmount = nums.length - shiftIndex;
+
+  while (popAmount > 0) {
+    nums.pop();
+    popAmount--;
+  }
+
+  nums.unshift(...tempArr);
+};
+
+//#endregion
+
+//#region --- Answer to Problem #15 ---
+export const firstRecurringCharacter = (arr: number[]): number | undefined => {
+  const myMap: {
+    [key: number]: boolean;
+  } = {};
+  let result: number | undefined = undefined;
+  let globalLowestIndex = Infinity;
+
+  for (let index = 0; index < arr.length; ++index) {
+    const value = arr[index];
+    if (!myMap[value]) {
+      myMap[value] = true;
+      continue;
+    }
+
+    const firstIndexOfValue = arr.indexOf(value);
+
+    if (firstIndexOfValue < globalLowestIndex) {
+      result = value;
+      globalLowestIndex = index;
+    }
+  }
+
+  return result;
+};
+//#endregion
+
+//#region  --- Answer to Problem #16 ---
+/*
+jumps -> 0
+index -> 0
+       👇
+arr -> [2, 4, 4, 1, 3, 3, 1, 1, 1]
+
+currentValue -> 2
+       
+most I can move is 2
+             👇
+arr -> [2, 4, 4, 1, 3, 3, 1, 1, 1]
+
+nextIndex -> index {0} + 1 -> 1
+
+array of posibilities -> arr.split(nextIndex {1}, nextIndex {1} + currentValue {2} {3}) -> [3, 1]
+largest number based on value and index is found at index 0 -> 3
+
+subIndex -> 0
+
+update index -> nextIndex {1} + subIndex {0} -> 1
+jumps++ -> 1
+
+IF index {1} > arr.length - 1 {4} -> NO
+    return jumps
+
+
+jumps -> 1
+index -> 1
+          👇
+arr -> [2, 4, 4, 1, 3, 3, 1, 1, 1]
+
+currentValue -> 3
+       
+most I can move is 3
+                   👇
+arr -> [2, 4, 4, 1, 3, 3, 1, 1, 1]
+
+nextIndex -> index {1} + 1 -> 2
+             👇
+arr -> [2, 4, 4, 1, 3, 3, 1, 1, 1]
+
+end -> nextIndex {2} + currentValue {4} -> 6
+
+IF end > arr.length -> YES
+    return ++jumps
+
+array of posibilities -> arr.split(nextIndex {2}, nextIndex {2} + currentValue {4} {6}) -> [4, 1, 1, 3] Remember that end is not inclusive
+arr.length -> 9
+
+largest number based on value and index is 3 -> 3 + 3 -> 6
+
+subIndex -> 3
+
+update index -> nextIndex {2} + subIndex {3} -> 5
+jumps++ -> 2
+
+IF index {1} > arr.length - 1 {6} -> NO
+    return jumps
+*/
+export const minimumJumps = (nums: number[]): number => {
+  if (nums.length <= 1) {
+    return 0;
+  }
+
+  let index = 0;
+  let jumps = 0;
+
+  while (index < nums.length) {
+    const currentValue = nums[index];
+    const start = index + 1;
+    const end = start + currentValue;
+
+    // This means that we can reach the end based on the current value
+    if (end >= nums.length) {
+      return ++jumps;
+    }
+
+    const options = nums.slice(start, end);
+
+    // Calculate the largest number in the posibilities based on it's value and index
+    let largestSize = -Infinity;
+    let subIndex = 0;
+    options.forEach((value, index1) => {
+      const size = value + index1;
+      if (size > largestSize) {
+        largestSize = size;
+        subIndex = index1;
+      }
+    });
+
+    index = start + subIndex;
+    jumps++;
+  }
+
+  return jumps;
+};
+
+export const minimumJumpsMostEfficient = (nums: number[]): number => {
+  let endOfJump = 0,
+    farthestJump = 0,
+    jumps = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    farthestJump = Math.max(farthestJump, i + nums[i]);
+    if (i === endOfJump) {
+      endOfJump = farthestJump;
+      jumps++;
+    }
+  }
+  return jumps;
+};
+//#endregion
+
+//#region --- Array Implementation ---
+export class MyArray {
+  length: number;
+  data: any = {};
+
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index: number) {
+    return this.data[index];
+  }
+
+  push(item: any) {
+    this.data[this.length] = item;
+    this.length++;
+  }
+
+  pop() {
+    const lastIndex = this.length - 1;
+    const lastItem = this.data[lastIndex];
+
+    delete this.data[lastIndex];
+    this.length--;
+
+    return lastItem;
+  }
+
+  delete(index: number) {
+    this.shiftItemsAtIndex(index);
+
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+  private shiftItemsAtIndex(index: number) {
+    for (let i = index; i < this.length; ++i) {
+      this.data[i] = this.data[i + 1];
+    }
+  }
+}
+//#endregion
+
+//#region Hash Table Implementation
+type HashTableData = [string, any];
+export class HashTable {
+  data: HashTableData[][];
+  constructor(size: number) {
+    this.data = new Array<[HashTableData]>(size);
+  }
+
+  private _hash(key: string): number {
+    let hash = 0;
+
+    for (let index = 0; index < key.length; ++index) {
+      hash = (hash + key.charCodeAt(index) * index) % this.data.length;
+    }
+
+    return hash;
+  }
+
+  set(key: string, value: any): void {
+    const hash = this._hash(key);
+    if (!this.data[hash]) {
+      const test: HashTableData = [key, value];
+      this.data[hash] = [test];
+
+      return;
+    }
+
+    const storedItem = this.data[hash].find(([dataKey]) => dataKey === key);
+
+    if (storedItem) {
+      storedItem[1] = value;
+      return;
+    }
+
+    this.data[hash].push([key, value]);
+  }
+
+  get(key: string) {
+    const hash = this._hash(key);
+    const dataArray = this.data[hash];
+
+    return dataArray?.find(([dataKey]) => dataKey === key)?.[1];
+  }
+
+  keys(): string[] {
+    return this.data.reduce((acc, currValue) => {
+      currValue.forEach(([key]) => acc.push(key));
+
+      return acc;
+    }, [] as string[]);
+  }
+}
+//#endregion
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,16 @@ import {
   reverseString,
   logPairs,
   containsCommonValue,
-  findPairWithSum
+  findPairWithSum,
+  mergeSortedArrays,
+  maxSubArray,
+  moveZeroesAlt,
+  containsDuplicate,
+  rotate,
+  MyArray,
+  HashTable,
+  firstRecurringCharacter,
+  minimumJumps
 } from './answers';
 
 /**
@@ -197,7 +206,7 @@ console.log(addThreeThenSquareEs5(6));
 console.log('--- Problem #6 ---');
 /*
 Write a function that reverses a string from scratch.
-Do not use built in reverse capabilities.
+Do not use built in reverse or sorting capabilities.
 */
 
 // #region YOUR CODE HERE
@@ -249,11 +258,302 @@ const problem9Array2: number[] = [1, 3, 2, 5];
 console.log(findPairWithSum(problem9Array1, 8)); // []
 console.log(findPairWithSum(problem9Array2, 8)); // [3, 5]
 
+console.log('--- Problem #10 ---');
 /*
-Closure gotcha questions.
+Write a function that takes in two sorted arrays and returns an
+array that contains all the values in the input arrays and it's
+sorted.
 */
+
+const problem10Array1: number[] = [0, 3, 4, 31];
+const problem10Array2: number[] = [4, 6, 30];
+
+// #region YOUR CODE HERE
+// #endregion
+
+console.log(
+  [0, 3, 4, 4, 6, 30, 31],
+  mergeSortedArrays(problem10Array1, problem10Array2)
+);
+
+console.log('--- Problem #11 ---');
+/*
+Given an integer array `nums`, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+*/
+
+const problem11Array1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const problem11Array2 = [1];
+const problem11Array3 = [5, 4, -1, 7, 8];
+
+// #region YOUR CODE HERE
+// #endregion
+
+console.log(6, maxSubArray(problem11Array1));
+console.log(1, maxSubArray(problem11Array2));
+console.log(23, maxSubArray(problem11Array3));
+
+console.log('--- Problem #12 ---');
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+
+Constraints:
+
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+
+*/
+
+const problem12Array1 = [0, 1, 0, 3, 12];
+const problem12Array2 = [0];
+const problem12Array3 = [0, 0, 1];
+
+// #region YOUR CODE HERE
+// #endregion
+
+moveZeroesAlt(problem12Array1);
+console.log([1, 3, 12, 0, 0], problem12Array1);
+moveZeroesAlt(problem12Array2);
+console.log([0], problem12Array2);
+moveZeroesAlt(problem12Array3);
+console.log([1, 0, 0], problem12Array3);
+
+console.log('--- Problem #13 ---');
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -109 <= nums[i] <= 109
+*/
+
+const problem13Array1 = [1, 2, 3, 1];
+const problem13Array2 = [1, 2, 3, 4];
+const problem13Array3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+
+// #region YOUR CODE HERE
+// #endregion
+
+console.log(true, containsDuplicate(problem13Array1));
+console.log(false, containsDuplicate(problem13Array2));
+console.log(true, containsDuplicate(problem13Array3));
+
+console.log('--- Problem #14 ---');
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -231 <= nums[i] <= 231 - 1
+    0 <= k <= 105
+
+
+Follow up:
+
+    Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+    Could you do it in-place with O(1) extra space?
+*/
+
+const problem14Array1 = [1, 2, 3, 4, 5, 6, 7];
+const problem14Array2 = [-1, -100, 3, 99];
+const problem14Array3 = [1, 2];
+const problem14Array4 = [1, 2];
+const problem14Array5 = [1, 2, 3];
+
+// #region YOUR CODE HERE
+// #endregion
+
+rotate(problem14Array1, 3);
+console.log([5, 6, 7, 1, 2, 3, 4], problem14Array1);
+rotate(problem14Array2, 2);
+console.log([3, 99, -1, -100], problem14Array2);
+rotate(problem14Array3, 3);
+console.log([2, 1], problem14Array3);
+rotate(problem14Array4, 2);
+console.log([1, 2], problem14Array4);
+rotate(problem14Array5, 4);
+console.log([3, 1, 2], problem14Array5);
+
+console.log('--- Problem #15 ---');
+/*
+Write a function that finds the first character that appears more
+than once in an array.
+*/
+
+const problem15Array1 = [2, 5, 1, 2, 3, 5, 1, 2, 4];
+const problem15Array2 = [2, 1, 1, 2, 3, 5, 1, 2, 4];
+const problem15Array3 = [2, 5, 5, 2, 3, 5, 1, 2, 4];
+const problem15Array4 = [2, 3, 4, 5];
+
+// #region YOUR CODE HERE
+// #endregion
+
+console.log(2, firstRecurringCharacter(problem15Array1));
+console.log(1, firstRecurringCharacter(problem15Array2));
+console.log(2, firstRecurringCharacter(problem15Array3));
+console.log(undefined, firstRecurringCharacter(problem15Array4));
+
+console.log('--- Problem #16 ---');
+/*
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+
+    0 <= j <= nums[i] and
+    i + j < n
+
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+
+
+Constraints:
+
+    1 <= nums.length <= 104
+    0 <= nums[i] <= 1000
+    It's guaranteed that you can reach nums[n - 1].
+*/
+
+const problem16Array1 = [2, 3, 1, 1, 4];
+const problem16Array2 = [2, 3, 0, 1, 4];
+const problem16Array3 = [2, 4, 4, 1, 3, 3, 1, 1, 1];
+const problem16Array4 = [5, 1];
+
+// #region YOUR CODE HERE
+// #endregion
+
+console.log(2, minimumJumps(problem16Array1));
+console.log(2, minimumJumps(problem16Array2));
+console.log(3, minimumJumps(problem16Array3));
+console.log(1, minimumJumps(problem16Array4));
+
+/* --- Data Structure implementations from scratch --- */
+
+console.log('--- Array ---');
+/*
+Create your own Array class
+*/
+
+// #region YOUR CODE HERE
+// #endregion
+
+const myArray = new MyArray();
+myArray.push('0');
+myArray.push('1');
+myArray.push('2');
+myArray.push('3');
+myArray.push('4');
+myArray.push('5');
+
+myArray.delete(2);
+console.log(myArray);
+
+console.log('--- Hash Table ---');
+/*
+Create your own Hash Table class
+*/
+
+// #region YOUR CODE HERE
+// #endregion
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 9);
+myHashTable.set('grapes', 10009);
+console.log(1009, myHashTable.get('grapes'));
+console.log(9, myHashTable.get('apples'));
+console.log(undefined, myHashTable.get('oranges'));
+console.log(['grapes', 'apples'], myHashTable.keys());
+console.log(myHashTable);
+
+/* --- Closure gotcha questions --- */
 setTimeout(() => {
-  console.log('--- Timeout Problems ---');
+  console.log('--- Closure Problems ---');
 }, 900);
 
 for (var var1 = 0; var1 < 3; var1++) {
